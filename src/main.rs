d9d61fc627902efd972ca7657eec2a2d1fc24016
Sigurd Holsen <sigurd.holsen@cognite.com>
2024-05-13 15:46:46 +0200

    weekday and now

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,50 @@ use chrono::DateTime;
 use clap::Parser;
 
 #[derive(Parser, Debug, Clone)]
-struct App {
+struct Args {
     millis: Option<f64>,
 
     #[arg(short = 'l', long = "local", help = "Use local timezone")]
     use_local_timezone: bool,
+
+    #[arg(long = "weekday", short = 'w', help = "Print week day")]
+    print_week_day: bool,
+
+    #[arg(short, long, help = "Use current time")]
+    now: bool,
 }
 
 fn main() {
-    let args = App::parse();
+    let args = Args::parse();
 
-    if let Some(millis) = args.millis {
+    if args.now {
+        let utc = Utc::now();
+        let utc = utc
+            .with_nanosecond(utc.nanosecond() / 1_000_000 * 1_000_000)
+            .unwrap();
+        print_time(utc, &args);
+    } else if let Some(millis) = args.millis {
         let utc = DateTime::from_timestamp_millis(millis as i64).unwrap();
-
-        if args.use_local_timezone {
-            let local = utc.with_timezone(&Local);
-            println!("{local:?}")
-        } else {
-            println!("{utc:?}");
-        }
+        print_time(utc, &args);
     } else {
         let now = Utc::now();
         println!("{}", now.timestamp_millis())
+    }
+}
+
+fn print_time(utc: DateTime<Utc>, args: &Args) {
+    if args.use_local_timezone {
+        let local = utc.with_timezone(&Local);
+        if args.print_week_day {
+            println!("{} {:?}", local.weekday(), local)
+        } else {
+            println!("{local:?}")
+        }
+    } else {
+        if args.print_week_day {
+            println!("{} {:?}", utc.weekday(), utc)
+        } else {
+            println!("{utc:?}")
+        }
     }
 }
