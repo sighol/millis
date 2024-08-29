01b6555217f2bea12c8865bf7499c57e88952529
Sigurd Holsen <sigurd.holsen@cognite.com>
2024-08-29 09:53:06 +0200

    minutes and parse_string

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,24 @@ struct Args {
     #[arg(long = "weekday", short = 'w', help = "Print week day")]
     print_week_day: bool,
 
+    #[arg(long = "parse-string", short = 'p', help = "Parse input as an ISO8601 string")]
+    parse_string: Option<String>,
+
     #[arg(short, long, help = "Use current time")]
     now: bool,
+
+    #[arg(short, long, help = "Print time in minutes")]
+    minutes: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
-    if args.now {
+    if let Some(s) = args.parse_string {
+        let time = DateTime::parse_from_rfc3339(&s).unwrap();
+        let millis = time.timestamp_millis();
+        println!("{}", millis);
+    } else if args.now {
         let utc = Utc::now();
         let utc = utc
             .with_nanosecond(utc.nanosecond() / 1_000_000 * 1_000_000)
@@ -30,7 +40,11 @@ fn main() {
         print_time(utc, &args);
     } else {
         let now = Utc::now();
-        println!("{}", now.timestamp_millis())
+        if args.minutes {
+            println!("{}", now.timestamp_millis() / 1000 / 60)
+        } else {
+            println!("{}", now.timestamp_millis())
+        }
     }
 }
 
